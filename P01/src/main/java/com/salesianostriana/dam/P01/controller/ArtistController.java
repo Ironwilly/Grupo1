abf22de779ca7ea71590c7f7a4d1c8bec997d221
Sergio Chamorro Garcia <chamorrogarciasergio@gmail.com>
2021-10-04 18:09:27 +0200

    Añadida petición PUT Editar Artista

---
+++ P01/src/main/java/com/salesianostriana/dam/P01/controller/ArtistController.java
@@ -0,0 +1,26 @@
+package com.salesianostriana.dam.P01.controller;
+
+import com.salesianostriana.dam.P01.model.Artist;
+import com.salesianostriana.dam.P01.repos.ArtistRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/artist")
+@RequiredArgsConstructor
+public class ArtistController {
+
+    private final ArtistRepository repository;
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Artist> edit(@RequestBody Artist artist, @PathVariable Long id){
+        return ResponseEntity.of(
+            repository.findById(id).map(a -> {
+                a.setName(artist.getName());
+                repository.save(a);
+                return a;
+            })
+        );
+    }
+}
