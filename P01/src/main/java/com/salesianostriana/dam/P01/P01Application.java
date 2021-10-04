6ec75553bb0c134cffe3dd0217904ae0559646fd
Sergio Chamorro Garcia <chamorrogarciasergio@gmail.com>
2021-10-04 17:28:10 +0200

    Creado proyecto P01 con estructura de paquetes a seguir

---
+++ P01/src/main/java/com/salesianostriana/dam/P01/P01Application.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.P01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P01Application.class, args);
+	}
+
+}
---
+++ P01/src/test/java/com/salesianostriana/dam/P01/P01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.P01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
