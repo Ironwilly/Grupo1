f136e2f78dd865ef8df14f0c3ec72670119fdca7
Moisés Miranda Corrales <moisesmiranda135@gmail.com>
2021-10-04 19:08:47 +0200

    se realiza el método post de artist

--- P01/src/main/java/com/salesianostriana/dam/P01/controller/ArtistController.java
+++ P01/src/main/java/com/salesianostriana/dam/P01/controller/ArtistController.java
@@ -3,6 +3,7 @@ package com.salesianostriana.dam.P01.controller;
 import com.salesianostriana.dam.P01.model.Artist;
 import com.salesianostriana.dam.P01.repos.ArtistRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +13,15 @@ import org.springframework.web.bind.annotation.*;
 public class ArtistController {
 
     private final ArtistRepository repository;
+
+    @PostMapping("/")
+    public ResponseEntity<Artist> create(@RequestBody Artist nuevo) {
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(repository.save(nuevo));
+
+    }
 
     @PutMapping("/{id}")
     public ResponseEntity<Artist> edit(@RequestBody Artist artist, @PathVariable Long id){
