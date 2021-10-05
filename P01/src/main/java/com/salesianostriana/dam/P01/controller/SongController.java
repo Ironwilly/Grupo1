e4eb56794d30b2ed63a9f054310f8f21543f450d
Guillermo <claseswilly@gmail.com>
2021-10-05 19:33:31 +0200

    Se realiza el método get(/id) de song

--- P01/src/main/java/com/salesianostriana/dam/P01/controller/SongController.java
+++ P01/src/main/java/com/salesianostriana/dam/P01/controller/SongController.java
@@ -1,7 +1,11 @@
 package com.salesianostriana.dam.P01.controller;
 
+import com.salesianostriana.dam.P01.model.Song;
 import com.salesianostriana.dam.P01.repos.SongRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class SongController {
 
     private final SongRepository repository;
+
+    @GetMapping("/{id}")
+
+    public ResponseEntity<Song> findOne(@PathVariable Long id) {
+
+        return ResponseEntity.of(repository.findById(id));
+    }
 
 }
