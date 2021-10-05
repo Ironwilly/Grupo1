ef8a3956b4daf91f2fbb901534cd68168388b4c8
Ironwilly <ferrari.fegui21@triana.salesianos.edu>
2021-10-05 14:29:12 +0200

    Ultimo cambio clases

--- P01/src/main/java/com/salesianostriana/dam/P01/controller/SongController.java
+++ P01/src/main/java/com/salesianostriana/dam/P01/controller/SongController.java
@@ -1,5 +1,6 @@
 package com.salesianostriana.dam.P01.controller;
 
+import com.salesianostriana.dam.P01.repos.SongRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/song")
 @RequiredArgsConstructor
 public class SongController {
+
+    private final SongRepository repository;
+
 }
