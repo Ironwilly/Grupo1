41aec20a0f5311c99ef1ccc212a849bef1a22439
Moisés Miranda Corrales <moisesmiranda135@gmail.com>
2021-10-05 14:13:36 +0200

    Se corrigen errores

--- P01/src/main/java/com/salesianostriana/dam/P01/controller/SongController.java
+++ P01/src/main/java/com/salesianostriana/dam/P01/controller/SongController.java
@@ -1,4 +1,11 @@
 package com.salesianostriana.dam.P01.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/song")
+@RequiredArgsConstructor
 public class SongController {
 }
--- P01/src/main/java/com/salesianostriana/dam/P01/model/Song.java
+++ P01/src/main/java/com/salesianostriana/dam/P01/model/Song.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -19,6 +17,8 @@ public class Song {
 
     private String tittle;
 
+    
+    @ManyToOne
     private Artist artist;
 
     private String album;
