2b0ea92e36b514a9c6224ee20b4ef6fd74079a57
Manuel Caro Delgado <caro.deman20@triana.salesianos.edu>
2021-10-05 14:25:56 +0200

    sin completar aun

--- P01/src/main/java/com/salesianostriana/dam/P01/controller/SongController.java
+++ P01/src/main/java/com/salesianostriana/dam/P01/controller/SongController.java
@@ -7,5 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/song")
 @RequiredArgsConstructor
+
 public class SongController {
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Song> edit (
+           Request
+    )
 }
