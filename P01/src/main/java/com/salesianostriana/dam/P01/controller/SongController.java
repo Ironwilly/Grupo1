6d487c6b96f9df887e95e3b28f82a88502bbc911
Manutwo <lans.rock.26@gmail.com>
2021-10-05 21:21:08 +0200

    feat-HU-09

--- P01/src/main/java/com/salesianostriana/dam/P01/controller/SongController.java
+++ P01/src/main/java/com/salesianostriana/dam/P01/controller/SongController.java
@@ -1,17 +1,38 @@
 package com.salesianostriana.dam.P01.controller;
 
+import com.salesianostriana.dam.P01.model.Artist;
+import com.salesianostriana.dam.P01.model.Song;
+import com.salesianostriana.dam.P01.repos.SongRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/song")
 @RequiredArgsConstructor
-
 public class SongController {
 
+    private final SongRepository repository;
+
+    @GetMapping("/{id}")
+
+    public ResponseEntity<Song> findOne(@PathVariable Long id) {
+
+        return ResponseEntity.of(repository.findById(id));
+    }
+
     @PutMapping("/{id}")
-    public ResponseEntity<Song> edit (
-           Request
-    )
+    public ResponseEntity<Song> edit(@RequestBody Song song, @PathVariable Long id){
+        return ResponseEntity.of(
+                repository.findById(id).map(s -> {
+                    s.setArtist(song.getArtist());
+                    s.setAlbum(song.getAlbum());
+                    s.setYear(song.getYear());
+                    return s;
+                })
+        );
+    }
+
+    
+
 }
