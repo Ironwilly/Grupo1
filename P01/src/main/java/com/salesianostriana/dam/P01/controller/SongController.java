fd47c03ea64a35acae9bda5bc457779c0c099579
Sergio Chamorro Garcia <chamorrogarciasergio@gmail.com>
2021-10-05 14:25:13 +0200

    Comienzo petición POST

--- P01/src/main/java/com/salesianostriana/dam/P01/controller/SongController.java
+++ P01/src/main/java/com/salesianostriana/dam/P01/controller/SongController.java
@@ -1,11 +1,26 @@
 package com.salesianostriana.dam.P01.controller;
 
+import com.salesianostriana.dam.P01.model.Song;
+import com.salesianostriana.dam.P01.repos.SongRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/song")
+@RequestMapping("/songs")
 @RequiredArgsConstructor
 public class SongController {
+
+    private final SongRepository repository;
+
+    @PostMapping("/")
+    public ResponseEntity<Song> create(@RequestBody Song nueva){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .build();
+    }
 }
