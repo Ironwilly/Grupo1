eeb911d10d13acca9b816c0f4fbc1b2a6d05e214
Moisés Miranda Corrales <moisesmiranda135@gmail.com>
2021-10-05 20:17:11 +0200

    Se arregla error en la lista de canciones, añadiendo 2 anotaciones

--- P01/src/main/java/com/salesianostriana/dam/P01/model/Playlist.java
+++ P01/src/main/java/com/salesianostriana/dam/P01/model/Playlist.java
@@ -4,9 +4,7 @@ package com.salesianostriana.dam.P01.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
@@ -20,6 +18,8 @@ public class Playlist {
     private Long id;
     private String name;
     private String descripcion;
+    @Column
+    @ElementCollection
     private List<Song> listSongs;
 
 
