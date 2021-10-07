c0fc120914827b1d4b26a7f88c06befeea04cb59
Sergio Chamorro Garcia <chamorrogarciasergio@gmail.com>
2021-10-07 23:49:51 +0200

    Añadidos comentarios en ArtistController y P01_Application

--- P01/src/main/java/com/salesianostriana/dam/P01/P01Application.java
+++ P01/src/main/java/com/salesianostriana/dam/P01/P01Application.java
@@ -1,9 +1,21 @@
 package com.salesianostriana.dam.P01;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(description = "API para controlar la app Trianafy",
+		version = "1.0",
+		contact = @Contact(email = "chamorro.gaser21@triana.salesianos.edu", name = "Sergio Chamorro García"),
+		license = @License(name = "CreativeCommons 3.0"),
+		title = "API Trianafy"
+)
+)
 public class P01Application {
 
 	public static void main(String[] args) {
--- P01/src/main/java/com/salesianostriana/dam/P01/controller/ArtistController.java
+++ P01/src/main/java/com/salesianostriana/dam/P01/controller/ArtistController.java
@@ -3,6 +3,9 @@ package com.salesianostriana.dam.P01.controller;
 import com.salesianostriana.dam.P01.model.Artist;
 import com.salesianostriana.dam.P01.repos.ArtistRepository;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -16,17 +19,21 @@ import java.util.List;
 @RestController
 @RequestMapping("/artist")
 @RequiredArgsConstructor
-@Tag(name = "Artista", description = "El controlador de Artistas")
+@Tag(name = "Artista", description = "Controlador de Artistas")
 public class ArtistController {
 
     private final ArtistRepository repository;
 
     @Operation(summary = "Obtiene una lista de todos los artistas")
     @ApiResponses(value = {
-        @ApiResponse
-    }
-
-    )
+        @ApiResponse(responseCode = "200",
+            description = "Se ha encontrado la lista de artistas",
+            content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Artist.class))}),
+        @ApiResponse(responseCode = "400",
+            description = "No se ha encontrado ningún artista",
+            content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Artist.class))})})
     @GetMapping("/")
     public ResponseEntity<List<Artist>> findAll(){
         if(repository.findAll().isEmpty()){
@@ -39,6 +46,16 @@ public class ArtistController {
                 .body(repository.findAll());
     }
 
+    @Operation(summary = "Crea un nuevo artista")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    description = "Se ha creado el artista",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Artist.class))}),
+            @ApiResponse(responseCode = "400",
+                    description = "No se ha podido crear el artista",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Artist.class))})})
     @PostMapping("/")
     public ResponseEntity<Artist> create(@RequestBody Artist nuevo) {
         return ResponseEntity
@@ -46,8 +63,20 @@ public class ArtistController {
                 .body(repository.save(nuevo));
     }
 
+    @Operation(summary = "Edita los atributos de un artista previamente creado")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Se ha encontrado el artista y se ha modificado",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Artist.class))}),
+            @ApiResponse(responseCode = "400",
+                    description = "No se ha encontrado al artista indicado",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Artist.class))})})
     @PutMapping("/{id}")
-    public ResponseEntity<Artist> edit(@RequestBody Artist artist, @PathVariable Long id){
+    public ResponseEntity<Artist> edit(@RequestBody Artist artist,
+                                       @Parameter(description = "ID del artista a buscar")
+                                       @PathVariable Long id){
         return ResponseEntity.of(
             repository.findById(id).map(a -> {
                 a.setName(artist.getName());
@@ -57,15 +86,33 @@ public class ArtistController {
         );
     }
 
+    @Operation(summary = "Obtiene un artista por su ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Se ha encontrado el artista indicado",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Artist.class))}),
+            @ApiResponse(responseCode = "400",
+                    description = "No se ha encontrado al artista indicado",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Artist.class))})})
     @GetMapping("/{id}")
-    public ResponseEntity<Artist> findOne (@PathVariable Long id) {
+    public ResponseEntity<Artist> findOne (@Parameter(description = "ID del artista a buscar")
+                                               @PathVariable Long id) {
         return ResponseEntity
                 .of(repository.findById(id));
     }
 
 
+    @Operation(summary = "Borra a un artista")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204",
+                    description = "El artista se ha borrado correctamente",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Artist.class))})})
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable Long id) {
+    public ResponseEntity<?> delete(@Parameter(description = "ID del artista a borrar")
+                                        @PathVariable Long id) {
         repository.deleteById(id);
         return ResponseEntity.noContent().build();
     }
