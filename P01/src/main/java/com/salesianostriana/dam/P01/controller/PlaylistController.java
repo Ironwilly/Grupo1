c28be77d9d488c966e3e912ce8f96a5ef1ffc739
Sergio Chamorro Garcia <chamorrogarciasergio@gmail.com>
2021-10-07 12:30:17 +0200

    Creación de POST song en playlist sin terminar

--- P01/src/main/java/com/salesianostriana/dam/P01/controller/PlaylistController.java
+++ P01/src/main/java/com/salesianostriana/dam/P01/controller/PlaylistController.java
@@ -2,8 +2,12 @@ package com.salesianostriana.dam.P01.controller;
 
 
 
+import com.salesianostriana.dam.P01.dto.CreatePlaylistDto;
+import com.salesianostriana.dam.P01.dto.PlaylistDtoConverter;
 import com.salesianostriana.dam.P01.model.Playlist;
+import com.salesianostriana.dam.P01.model.Song;
 import com.salesianostriana.dam.P01.repos.PlaylistRepository;
+import com.salesianostriana.dam.P01.repos.SongRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,21 +21,22 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PlaylistController {
 
-    private final PlaylistRepository repository;
+    private final PlaylistRepository playlistRepository;
+    private final SongRepository songRepository;
+    private final PlaylistDtoConverter converter;
 
     @GetMapping("/")
     public ResponseEntity<List<Playlist>> findAll(){
 
-        return ResponseEntity.ok().body(repository.findAll());
-
-
+        return ResponseEntity.ok().body(playlistRepository.findAll());
     }
   
-  @PostMapping("/")
-    public ResponseEntity<Playlist> create(@RequestBody Playlist nuevo) {
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(repository.save(nuevo));
+    @PostMapping("/")
+    public ResponseEntity<Playlist> create(@RequestBody CreatePlaylistDto dto) {
+        if(dto.getSongId() == null){
+            return ResponseEntity.badRequest().build();
+        }
+        
     }
 
 
